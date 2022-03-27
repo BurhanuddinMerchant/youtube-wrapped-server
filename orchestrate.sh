b0f0131f0807c10725795bbb9194c1055150690e
Burhanuddin Merchant <bmerchant945@gmail.com>
2022-03-28 00:18:17 +0530

    added horizontal autoscaler

--- orchestrate.sh
+++ orchestrate.sh
@@ -1,3 +1,4 @@
 kubectl create secret generic dev-secrets --from-env-file=dev.env
 kubectl apply -f deployment.yaml
 kubectl apply -f service.yaml
+kubectl apply -f hpa.yaml
--- stop_orchestrate.sh
+++ stop_orchestrate.sh
@@ -1,3 +1,4 @@
 kubectl delete -f deployment.yaml
 kubectl delete -f service.yaml
+kubectl delete -f hpa.yaml
 kubectl delete  secrets/dev-secrets
